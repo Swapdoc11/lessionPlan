01dbaf2b449796bf8e5b14b51af2c208656d2a9a
shadow <swapdoc11@rediff>
2022-09-07 23:43:10 +0530

    commit

---
+++ src/components/auth/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Register from "./Register";
+
+const Login = () => {
+    var [username, setUsername] = useState();
+    var [password, setPassword] = useState();
+    var [isRegister,setRegister] = useState(false)
+
+    const loginUser = (username, password) => {
+        if (username === "swapnil" && password === "jadhav") {
+            console.warn("Hello School App")
+        }
+    }
+
+   
+
+    return (
+        <>
+    
+            <h2>LOGIN</h2>
+            <div>
+                
+    
+                <input type='text' onChange={(e) => setUsername(e.target.value)} placeholder="Username" /><br /><br />
+                <input type='password' onChange={(e) => setPassword(e.target.value)} placeholder="Password" /><br /><br />
+                <button onClick={() => loginUser(username, password)}>Submit</button> &nbsp;
+                <nav>
+                <Link to='/Register'>Register</Link>
+                </nav>
+                
+                <button onClick={() => setRegister(true)}>Register</button>
+                
+                {isRegister? <Register />:null}
+            </div>
+        </>
+    )
+}
+export default Login;
---
+++ src/components/auth/Register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div>
+            <h2> Register </h2>
+            <input type='text' placeholder='Aadhar No..' /> <br /><br />
+            <input type='text' placeholder='Name' /> <br /><br />
+            
+            <button>Register</button>
+        </div>
+    )
+}
+
+export default Register;
